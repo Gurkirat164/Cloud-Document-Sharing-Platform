745dc9c8acfcb37f62bbdcf0bf8ac641fde9f165
Gurkirat Singh <ggurkiratssingh1@gmail.com>
2026-04-03 12:35:52 +0530

    file restructuring and added database schema

--- Frontend/src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-export { LoginPage } from './LoginPage';
-export { RegisterPage } from './RegisterPage';
-export { DashboardPage } from './DashboardPage';
-export { FilesPage } from './FilesPage';
-export { UploadPage } from './UploadPage';
-export { ActivityPage } from './ActivityPage';
-export { AccessControlPage } from './AccessControlPage';
-export { SettingsPage } from './SettingsPage';
